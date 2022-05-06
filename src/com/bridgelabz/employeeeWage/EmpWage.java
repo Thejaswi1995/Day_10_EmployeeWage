0f93c11face8d51d17dcc0accfddc21f6666983d
Thejaswi <thejaswirl187@gmail.com>
2022-05-06 15:38:10 +0530

    Added : Employee Wage Builder

--- src/com/bridgelabz/employeeeWage/EmpWage.java
+++ src/com/bridgelabz/employeeeWage/EmpWage.java
@@ -1,42 +1,87 @@
 package com.bridgelabz.employeeeWage;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class EmpWage
-{   static int empWagePerHour = 20;
-    static int fullTimeHours = 0;
-    static int partTimeHours = 0;
-    static int workingDaysPerMonth = 0;
-    static int monthlyWorkHours = 0;
-    int totalEmpHrs;
-
-    {
-        totalEmpHrs = 0;
-    }
-
+{
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static void computeEmpWage (String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-        //Variables
-        int empHrs = 0;
+    int wagePerHour;
+    int fullDayHour;
+    int partTimeHour;
+    int workingDaysPerMonth;
+    int workingHoursPerMonth;
+    int totalWorkingHours;
+    int totalEmpWage;
+    String companyName;
+    ArrayList<Integer> dailyWage = new ArrayList<>();
 
-                //Computation
-                while ( totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
-                    totalWorkingDays++;
-                    int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-                    empHrs = switch (empCheck) {
-                        case IS_PART_TIME -> 4;
-                        case IS_FULL_TIME -> 8;
-                        default -> 0;
-                    };
-                    totalEmpHrs += empHrs;
-                    System.out.println("On Day " + totalWorkingDays + " Emp Hours is :" + empHrs);
+    public EmpWage (String companyName, int wagePerHour, int workingDaysPerMonth, int workingHoursPerMonth, int totalWorkingHours, int fullDayHour, int partTimeHour)
+    {
+        this.companyName = companyName;
+        this.wagePerHour = wagePerHour;
+        this.workingDaysPerMonth = workingDaysPerMonth;
+        this.workingHoursPerMonth = workingHoursPerMonth;
+        this.totalWorkingHours = totalWorkingHours;
+        this.fullDayHour = fullDayHour;
+        this.partTimeHour = partTimeHour;
+    }
+
+    public void computeEmpWage()
+    {
+        int empWage = 0;
+
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+
+        while (totalEmpHrs <= workingHoursPerMonth && totalWorkingDays < workingDaysPerMonth)
+        {
+            totalWorkingDays++;
+            Random num = new Random();
+            int empCheck = num.nextInt(2)+1;
+
+            if( empCheck == IS_PART_TIME)
+                if (totalWorkingHours + partTimeHour <= workingHoursPerMonth)
+                {
+                    empWage = wagePerHour * partTimeHour;
+                    totalWorkingHours = totalWorkingHours + partTimeHour;
                 }
-                int totalEmpWage = totalEmpHrs * int wagePerHour;
-                System.out.println("Total Emp Wage for the company: " + companyName+" is :" + totalEmpWage);
-
-
-            }
-            public static void main (String[]args)
-            {
-                computeEmpWage("PayPal", 40, 25, 120);
-
+            if ( empCheck == IS_FULL_TIME)
+                if (totalWorkingHours + fullDayHour <= workingHoursPerMonth)
+                {
+                    empWage = wagePerHour * fullDayHour;
+                    totalWorkingHours = totalWorkingHours + fullDayHour;
+                }
+            dailyWage.add(empWage);
+            totalEmpWage = totalEmpWage + empWage;
+            totalWorkingDays++;
+            System.out.println("Total Wage for Day" + (totalWorkingDays) + " is : " + totalEmpWage);
         }
+        System.out.println("Total Working Days :" + totalWorkingDays);
+        System.out.println("Total Working Hours :" + totalWorkingHours);
+        System.out.println("Employee Wage :" + totalEmpWage);
+    }
+    @Override
+    public String toString() {
+        return "EmployeeWage{" +
+                "company: " + companyName +
+                ", wagePerHour=" + wagePerHour +
+                ", fullDayHour=" + fullDayHour +
+                ", partTimeHour=" + partTimeHour +
+                ", workingDaysPerMonth=" + workingDaysPerMonth +
+                ", workingHoursPerMonth=" + workingHoursPerMonth +
+                ", dailyWageArray=" + dailyWage +
+                ", totalEmpWage=" + totalEmpWage +
+                '}';
+    }
+
+    public static void main(String[] args)
+    {
+        EmpWageBuilder.createEmployeeWage();
+        for (Object employeeObject : EmpWageBuilder.array)
+        {
+            System.out.println(employeeObject);
+        }
+    }
+}
