e57b7e390eb9c75105e0815c23c234d9124fef28
Thejaswi <thejaswirl187@gmail.com>
2022-05-06 15:34:32 +0530

    Added : Saving Emplyee Wage for each company

--- src/com/bridgelabz/employeeeWage/EmpWage.java
+++ src/com/bridgelabz/employeeeWage/EmpWage.java
@@ -1,20 +1,25 @@
 package com.bridgelabz.employeeeWage;
 
 public class EmpWage
-{
+{   static int empWagePerHour = 20;
+    static int fullTimeHours = 0;
+    static int partTimeHours = 0;
+    static int workingDaysPerMonth = 0;
+    static int monthlyWorkHours = 0;
+    int totalEmpHrs;
+
+    {
+        totalEmpHrs = 0;
+    }
+
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static void computeEmpWage (String companyName,int empRatePerHour, int numOfWorkingDays,
-            int maxHoursPerMonth)
-            {
-                //Variables
-                int empHrs = 0;
-                int totalEmpHrs = 0;
-                int totalWorkingDays = 0;
+    public static void computeEmpWage (String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+        //Variables
+        int empHrs = 0;
 
                 //Computation
-                while (totalEmpHrs <= maxHoursPerMonth &&
-                        totalWorkingDays < numOfWorkingDays) {
+                while ( totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
                     totalWorkingDays++;
                     int empCheck = (int) Math.floor(Math.random() * 10) % 3;
                     empHrs = switch (empCheck) {
@@ -25,15 +30,13 @@ public class EmpWage
                     totalEmpHrs += empHrs;
                     System.out.println("On Day " + totalWorkingDays + " Emp Hours is :" + empHrs);
                 }
-                int totalEmpWage = totalEmpHrs * empRatePerHour;
-                System.out.println("Total Emp Wage for the company: " + companyName + " is :" + totalEmpWage);
+                int totalEmpWage = totalEmpHrs * int wagePerHour;
+                System.out.println("Total Emp Wage for the company: " + companyName+" is :" + totalEmpWage);
 
 
             }
             public static void main (String[]args)
             {
                 computeEmpWage("PayPal", 40, 25, 120);
-                computeEmpWage("Tesla", 35, 25, 95);
-                computeEmpWage("Airtel", 35, 25, 100);
-            }
+
         }
